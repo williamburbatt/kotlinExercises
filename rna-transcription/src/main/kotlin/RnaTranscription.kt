8cf72a29983f26b8584af4ea4bb230808c21ce58
williamburbatt <williamburbatt@yahoo.com>
2018-04-25 10:16:38 -0500

    Finished Ex3

--- rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,27 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+    var fin = "";
+    var temp = dna
+    var finlength = temp.length
+    while(finlength > 0) {
+        when (temp.substring(0, 1)) {
+            "G" -> fin = fin + "C"
+            "C" -> fin = fin + "G"
+            "T" -> fin = fin + "A"
+            "A" -> fin = fin + "U"
+            else -> {
+                "Invalid Letter"
+            }
+
+        }
+        if(temp.length > 1)
+        temp = temp.substring(1)
+        else
+            break
+    }
+    return fin
+}
+
+fun main(args : Array<String>) {
+    var fin = transcribeToRna("ACGTGGTCTTAA")
+    println(fin)
+}
